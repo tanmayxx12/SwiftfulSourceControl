d53947e72c9532aaecb09992394ac6403b39f716
tanmayxx12 <troy12rollsza@gmail.com>
2024-10-18 16:48:41 +0530

    [Feature] Add Subscribe Button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,25 @@
 //  Created by Tanmay . on 18/10/24.
 //
 
+
+// COMMIT MESSAGES:
+/*
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of the patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of the changes
+ 
+ RELEASE:
+ [Release] Description of release
+ */
+
 import SwiftUI
 
 struct ContentView: View {
@@ -21,6 +40,12 @@ struct ContentView: View {
                 .font(.headline)
                 .buttonStyle(.borderedProminent)
                 .shadow(radius: 10, y: 10)
+            
+            Button("Subscribe") {}
+                .font(.headline)
+                .buttonStyle(.borderedProminent)
+                .shadow(radius: 10, y: 10)
+                .tint(.gray)
             
         }
         .padding()
