5a8bac8a92d2125fce6cc77fb98a60c42e582663
tanmayxx12 <troy12rollsza@gmail.com>
2024-10-18 18:35:56 +0530

    [Feature] Adding Texts to HomeView

--- HomeView.swift
+++ HomeView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("Screen 2")
+        VStack {
+            Text("This is the first text")
+                .font(.headline)
+                .foregroundStyle(.white)
+                .padding()
+                .background(.red)
+                .cornerRadius(10)
+            
+            Text("This is the second text")
+                .font(.headline)
+                .foregroundStyle(.white)
+                .padding()
+                .background(.blue)
+                .cornerRadius(10)
+        }
     }
 }
 
