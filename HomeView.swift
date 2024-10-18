fa07a2985d9e5d43d38eb113aedc8e871f4e1563
tanmayxx12 <troy12rollsza@gmail.com>
2024-10-18 16:36:41 +0530

    Add HomeView

---
+++ HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Tanmay . on 18/10/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
