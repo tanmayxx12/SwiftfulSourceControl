c5d8c3a2fb6096cb7b25bd4bbc3ca03e6b7fb3cd
tanmayxx12 <troy12rollsza@gmail.com>
2025-01-12 19:37:07 +0530

    [Feature] Add TextField, Save and Clear buttons

---
+++ SwiftfulSourceControl/GitPractice.swift
@@ -0,0 +1,18 @@
+//
+//  GitPractice.swift
+//  SwiftfulSourceControl
+//
+//  Created by Tanmay . on 12/01/25.
+//
+
+import SwiftUI
+
+struct GitPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GitPractice()
+}
