c6e92c5c4d697dfe04c28f9069c1ecd861bea14d
tanmayxx12 <troy12rollsza@gmail.com>
2025-01-12 19:39:02 +0530

    [Feature] Add TextField and some Buttons with a list of  names.

--- SwiftfulSourceControl/GitPractice.swift
+++ SwiftfulSourceControl/GitPractice.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct GitPractice: View {
+    @State private var textFieldText: String = ""
+    @State private var names: [String] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Your name...", text: $textFieldText)
+                .padding(.leading)
+                .frame(maxWidth: .infinity)
+                .frame(height: 50)
+                .background(.black.opacity(0.2))
+                .cornerRadius(10)
+                .padding(.horizontal, 8)
+                .autocorrectionDisabled()
+            
+            HStack {
+                Button("Save"){
+                    save()
+                }
+                
+                Button("Clear") {
+                    clear()
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.secondary)
+            .font(.headline)
+            
+            List {
+                ForEach(names, id: \.self) { name in
+                    withAnimation(.easeInOut) {
+                        Text(name)
+                    }
+                }
+            }
+            .listStyle(.plain)
+            
+            
+        }
+        .padding(5)
     }
+    
+    // Function to save name in the array:
+    func save() {
+        let name = textFieldText
+        names.append(name)
+        textFieldText = ""
+    }
+    
+    // Function to clear the list:
+    func clear() {
+        names.removeAll()
+        textFieldText = ""
+    }
+    
 }
 
 #Preview {
